986a01f666c95504ccc0be76b12e525434c012f9
Ajay Rajpurohit <ajaybrajpurohit.com>
2020-07-22 11:35:29 +0530

    Added port env veriable

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 //Define paths fpr Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -86,5 +86,5 @@ app.get('*',(req,res)=>{
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port 3000');
+    console.log('Server is up on port', port);
 })
